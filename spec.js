b960a4f906cd5c4aa0d9b7502f8ce28653804c45
Aidan Denahy <denahyaidan@gmail.com>
2023-01-19 21:05:44 -0500

    hi

--- spec.js
+++ spec.js
@@ -1,4 +1,9 @@
 const assert = require("assert")
+
+function pow (number, power) {
+  return Math.pow(number, power)
+}
+
 describe("pow", function() {
 
     it("raises to n-th power", function() {
